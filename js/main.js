9d8d06d16aec9c3c6a31421b0ede2732037f8448
oyatillo0201 <oyatillosohibov23@gmail.com>
2022-08-11 23:40:25 +0300

    Add modal

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let elLink = document.querySelector('.imgs__link');
+let elModal = document.querySelector('.modal');
+
+elLink.addEventListener('click', function() {
+  elModal.classList.add('modal-open')
+})
+
