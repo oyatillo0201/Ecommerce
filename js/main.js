4aea15dc2e29f3fcccbe58e0c7049e0141835082
oyatillo0201 <oyatillosohibov23@gmail.com>
2022-08-12 09:57:36 +0300

    Add section features

--- js/main.js
+++ js/main.js
@@ -5,3 +5,31 @@ elLink.addEventListener('click', function() {
   elModal.classList.add('modal-open')
 })
 
+
+let elPrev = document.querySelector('.button-pilus');
+let elNum = document.querySelector('.button-zero');
+let elNext =document.querySelector('.button-minus');
+
+
+elPrev.addEventListener('click', function() {
+  // elNum.innerText = Number(elNum.innerText) +1;
+
+  if(elNum.innerText == 20){
+    elNum.innerText = 20
+  } else {
+    elNum.innerText = Number(elNum.innerText) +1;
+  }
+})
+
+
+
+elNext.addEventListener('click', function() {
+  // elNum.innerText = Number(elNum.innerText) -1;
+
+  if(elNum.innerText == 0){
+    elNum.innerText = 0
+  } else{
+    elNum.innerText = Number(elNum.innerText) -1;
+  }
+
+})
