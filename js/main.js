3880c93b1225ee5049f60de394d8c33abb2d38e1
oyatillo0201 <oyatillosohibov23@gmail.com>
2022-08-13 21:44:15 +0300

    Update active showcase img on thumbnail click

--- js/main.js
+++ js/main.js
@@ -1,35 +1,43 @@
 let elLink = document.querySelector('.imgs__link');
 let elModal = document.querySelector('.modal');
 
-elLink.addEventListener('click', function() {
-  elModal.classList.add('modal-open')
-})
+const modifiers = {
+  imgThumbnailActive: "img-showcase__thumbnail--active"
+};
 
 
-let elPrev = document.querySelector('.button-pilus');
-let elNum = document.querySelector('.button-zero');
-let elNext =document.querySelector('.button-minus');
+// Shopping cart modal
+const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
+const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
 
+if(elSiteHeaderCartLink) {
+  elSiteHeaderCartLink.addEventListener('click', function(evt) {
+    evt.preventDefault();
 
-elPrev.addEventListener('click', function() {
-  // elNum.innerText = Number(elNum.innerText) +1;
-
-  if(elNum.innerText == 20){
-    elNum.innerText = 20
-  } else {
-    elNum.innerText = Number(elNum.innerText) +1;
-  }
-})
-
-
-
-elNext.addEventListener('click', function() {
-  // elNum.innerText = Number(elNum.innerText) -1;
-
-  if(elNum.innerText == 0){
-    elNum.innerText = 0
-  } else{
-    elNum.innerText = Number(elNum.innerText) -1;
-  }
+    elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
 
 })
+}
+
+
+// Image showcase
+const elImgShowcaseActiveImg = document.querySelector('.img-showcase__active-img');
+const elsImgShowcaseThumbnailButton = document.querySelectorAll('.js-img-showcase-thumbnail-button');
+const elsImgThumbnail = document.querySelectorAll('.img-showcase__thumbnail')
+
+elsImgShowcaseThumbnailButton.forEach(function(elButton) {
+  elButton.addEventListener('click', function() {
+    // Remove active state from all buttons
+    elsImgThumbnail.forEach(function (elImgThumbnail) {
+      elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
+    });
+
+    // Add active state to clicked button
+    elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+
+    // Update active image src accordingly
+    elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig;
+    elImgShowcaseActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetina} 2x`;
+    console.log(elButton.dataset)
+  });
+});
