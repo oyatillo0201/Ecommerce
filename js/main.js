ed369add45a448e4d589953265524d1fd72a64dd
oyatillo0201 <oyatillosohibov23@gmail.com>
2022-08-14 18:14:24 +0300

    Add Lightbox styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-let elLink = document.querySelector('.imgs__link');
-let elModal = document.querySelector('.modal');
+// let elLink = document.querySelector('.imgs__link');
+// let elModal = document.querySelector('.modal');
+
+// elLink.addEventListener('click', function() {
+//   elModal.classList.add('modal-open')
+// })
+
+
+// let elPrev = document.querySelector('.button-pilus');
+// let elNum = document.querySelector('.button-zero');
+// let elNext =document.querySelector('.button-minus');
+
+
+// elPrev.addEventListener('click', function() {
+//   // elNum.innerText = Number(elNum.innerText) +1;
+
+//   if(elNum.innerText == 20){
+//     elNum.innerText = 20
+//   } else {
+//     elNum.innerText = Number(elNum.innerText) +1;
+//   }
+// })
+
+
+
+// elNext.addEventListener('click', function() {
+//   // elNum.innerText = Number(elNum.innerText) -1;
+
+//   if(elNum.innerText == 0){
+//     elNum.innerText = 0
+//   } else{
+//     elNum.innerText = Number(elNum.innerText) -1;
+//   }
+
+// })
+
+
 
 const modifiers = {
   imgThumbnailActive: "img-showcase__thumbnail--active"
@@ -16,7 +51,7 @@ if(elSiteHeaderCartLink) {
 
     elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
 
-})
+  })
 }
 
 
